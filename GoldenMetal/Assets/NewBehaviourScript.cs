4b1d6efa84c203c1731b0875707d0fd4f10c9e60
wlsdn0317 <78064428+wlsdn0317@users.noreply.github.com>
2023-09-03 16:26:52 +0900

    test

---
+++ GoldenMetal/Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello Unity");
+
+
+        //1.변수
+        int level = 5;
+        float strength = 15.5f;
+        string playerName = "나검사";
+        bool isFullLevel = false;
+
+        Debug.Log("용사의 이름은?");
+        Debug.Log(playerName);
+        Debug.Log("용사의 레벨은?");
+        Debug.Log(level);
+        Debug.Log("용사의 힘은?");
+        Debug.Log(strength);
+        Debug.Log("용사는 만렙인가? 마지");
+        Debug.Log(isFullLevel);
+
+
+        
+    }
+}
